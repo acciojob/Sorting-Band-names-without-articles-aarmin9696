72cd28be8a92d5f0edd3eb08adb44450b936f568
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-13 17:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
 
+let bandNames = ["The Beatles", "Led Zeppelin", "The Rolling Stones", "Pink Floyd", "The Who", "Radiohead", "Aerosmith", "The Beach Boys", "The Doors", "Queen"];
+
+let cleanBandNames = bandNames.map(name => name.replace(/^(a |an |the )/i, ''))
+                              .sort();
+
+let ulTag = document.getElementById('band');
+ulTag.innerHTML = cleanBandNames.map(name => `<li>${name}</li>`).join('');
