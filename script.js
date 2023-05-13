9e5c7eaa574e4207ed1cdf9ea4f094c7e3cf9fd6
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-13 17:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 
 let bandNames = ["The Beatles", "Led Zeppelin", "The Rolling Stones", "Pink Floyd", "The Who", "Radiohead", "Aerosmith", "The Beach Boys", "The Doors", "Queen"];
 
-let cleanBandNames = bandNames.map(name => name.replace(/^(a |an |the )/i, ''))
-                              .sort();
+// Remove articles 'a', 'an', 'the' from band names
+let cleanBandNames = bandNames.map(name => name.replace(/^(a|an|the)\s+/i, ''));
 
+// Sort the clean band names in lexicographic order
+cleanBandNames.sort();
+
+// Create a list of li tags for each band name
+let listItems = cleanBandNames.map(name => `<li>${name}</li>`);
+
+// Add the list items to the ul tag with id='band'
 let ulTag = document.getElementById('band');
-ulTag.innerHTML = cleanBandNames.map(name => `<li>${name}</li>`).join('');
+ulTag.innerHTML = listItems.join('');
